38ebba47ddc44dbd2f17cf97704fbc8f176c7153
Jonathan Scott <jonathanscott1989@gmail.com>
2012-02-19 19:53:37 +0000

    got rid of _meta and created .children - in preparation for moving to objects

--- chart.js
+++ chart.js
@@ -10,7 +10,7 @@ function count_leading_tabs(line) {
 function load_indented_data(data) {
 	var lines = data.split('\n');
 
-	var obj = {}
+	var obj = {'children': {}}
 
 	load_indented_lines(obj, 0, lines, 0);
 	return obj;
@@ -31,27 +31,15 @@ function load_indented_lines(obj, level, lines, line_count) {
 		}
 
 		if (line[0] == level) {
-			obj[line[1]] = {}
+			obj.children[line[1]] = {'name': line[1], 'children': [], 'left': 0, 'width': 0, 'hidden': false, 'lines': []};
 
 			// Assume we can move in by one level and continue looking
-			line_count = load_indented_lines(obj[line[1]], level + 1, lines, line_count + 1)
+			line_count = load_indented_lines(obj.children[line[1]], level + 1, lines, line_count + 1)
 		} else {
 			return line_count;
 		}
 	}
 	return line_count;
-}
-
-function format_as_list(formatted_data) {
-	var ul = $('<ul></ul>');
-
-	for (i in formatted_data) {
-		var li = $('<li><div class="positioning"></div><span>' + i + '</span></li>');
-		li.append(format_as_list(formatted_data[i]));
-		ul.append(li);
-	}
-
-	return ul
 }
 
 function draw_line(paper, el, start_position, end_position) {
@@ -68,55 +56,47 @@ function draw_line(paper, el, start_position, end_position) {
 	c.attr('stroke-width', 3);
 	c.attr('stroke-opacity', 1);
 
-	el['_meta']['lines'].push(c);
+	el.lines.push(c);
 }
 
 var paper = Raphael(0, 0, 10000, 10000);
 
 function draw_lines(el) {
-	var inner_node = el['_meta']['node'].find('.inner-node');
+	var inner_node = el.element.find('.inner-node');
 	var start_position = inner_node.offset();
 	start_position.left += (inner_node.width()/2);
 	start_position.top += inner_node.height();
 
 	var line;
-	while (line = el['_meta']['lines'].pop()) {
+	while (line = el.lines.pop()) {
 		line.remove();
 	}
 
-	for(var i in el) {
-		if (i != '_meta') {
-			if (el[i]['_meta']['node'].is(':visible')) {
-				inner_node = el[i]['_meta']['node'].find('.inner-node');
-				var end_position = inner_node.offset();
-				end_position.left += inner_node.width()/2;
-				draw_line(paper, el, start_position, end_position);
-			}
-			draw_lines(el[i]);
+	for(var i in el.children) {
+		if (el.children[i].element.is(':visible')) {
+			inner_node = el.children[i].element.find('.inner-node');
+			var end_position = inner_node.offset();
+			end_position.left += inner_node.width()/2;
+			draw_line(paper, el, start_position, end_position);
 		}
+		draw_lines(el.children[i]);
 	}
 }
 
 function calculate_width(el, caption) {
-	var width = 0;
+	el.width = 0;
 
-	if (!('_meta' in el) || !('hidden' in el['_meta']) || !(el['_meta']['hidden'])) {
-		for(var i in el) {
-			if (i != '_meta') {
-				width += calculate_width(el[i], i);
-			}
+	if (!(el.hidden)) {
+		for(var i in el.children) {
+			el.width += calculate_width(el.children[i], i);
 		}
 	}
 
-	if (width < 210) {
-		width = 210;
-	}
-	if (!('_meta' in el)) {
-		el['_meta'] = {'lines': []};
+	if (el.width < 210) {
+		el.width = 210;
 	}
 
-	el['_meta']['width'] = width;
-	return width;
+	return el.width;
 }
 
 // Returns true if the positions have changed
@@ -125,23 +105,22 @@ function calculate_positions(el, level) {
 		level = 0;
 	}
 
-	var ret = false;
+	var position_changed = false;
 
-	var left = el['_meta']['left'];
+	var left = el.left;
 
-	for(var i in el) {
-		if (i != '_meta') {
-			if ((!('left' in el[i]['_meta'])) || (el[i]['_meta']['left'] != left)) {
-				el[i]['_meta']['left'] = left;
-				ret = true;
-			}
-			el[i]['_meta']['level'] = level;
-			left += el[i]['_meta']['width'];
-			ret = calculate_positions(el[i], level + 1) || ret;
+	for(var i in el.children) {
+		if (el.children[i].left != left) {
+			el.children[i].left = left;
+			position_changed = true;
 		}
+
+		el.children[i].level = level;
+		left += el.children[i].width;
+		position_changed = calculate_positions(el.children[i], level + 1) || position_changed;
 	}
 
-	return ret;
+	return position_changed;
 }
 
 function count(obj) {
@@ -162,94 +141,91 @@ var BASE_LEFT = 200;
 function place_nodes(el, name) {
 	node_count += 1;
 	if (name) {
-		var node;
 		if (count(el) > 1) { 
-			node = $('#nodeTemplateChildren').tmpl({"name": name}).css({"left": BASE_LEFT + el['_meta']['left'], "width": el['_meta']['width'], "top": BASE_LEVEL + el['_meta']['level'] * LEVEL_HEIGHT}).appendTo($('body'));
+			el.element = $('#nodeTemplateChildren').tmpl({"name": name}).css({
+				"left": BASE_LEFT + el.left, 
+				"width": el.width,
+				"top": BASE_LEVEL + el.level * LEVEL_HEIGHT
+			}).appendTo($('body'));
 		} else {
-			node = $('#nodeTemplate').tmpl({"name": name}).css({"left": BASE_LEFT + el['_meta']['left'], "width": el['_meta']['width'], "top": BASE_LEVEL + el['_meta']['level'] * LEVEL_HEIGHT}).appendTo($('body'));
+			el.element = $('#nodeTemplate').tmpl({"name": name}).css({
+				"left": BASE_LEFT + el.left, 
+				"width": el.width, 
+				"top": BASE_LEVEL + el.level * LEVEL_HEIGHT
+			}).appendTo($('body'));
 		}
-		node.data('node', el);
-		el['_meta']['node'] = node;
+		el.element.data('node', el);
 	}
-	for(var i in el) {
-		if (i != '_meta') {
-			place_nodes(el[i], i);
-		}
+	for(var i in el.children) {
+		place_nodes(el.children[i], i);
 	}
 }
 
 function position_nodes(el, name, callback) {
 	if (name) {
-		var node = el['_meta']['node'];
-		node.animate({"left": BASE_LEFT + el['_meta']['left'], "width": el['_meta']['width'], "top": BASE_LEVEL + el['_meta']['level'] * LEVEL_HEIGHT}, {"complete": callback});
+		el.element.animate({
+			"left": BASE_LEFT + el.left, 
+			"width": el.width, 
+			"top": BASE_LEVEL + el.level * LEVEL_HEIGHT
+		}, {"complete": callback});
 	}
-	for(var i in el) {
-		if (i != '_meta') {
-			position_nodes(el[i], i, callback);
-		}
+	for(var i in el.children) {
+		position_nodes(el.children[i], i, callback);
 	}
 }
 
 function refresh_visibility(node, hidden_in_tree) {
-	if ('_meta' in node && 'node' in node['_meta']) {
+	if ('element' in node) {
 		if (hidden_in_tree) {
-			node['_meta']['node'].hide();
+			node.element.hide();
 		} else {
-			node['_meta']['node'].show();
+			node.element.show();
 		}
 
-		if (node['_meta']['hidden']) {
-			node['_meta']['node'].find('.expand').text('v');
+		if (node.hidden) {
+			node.element.find('.expand').text('v');
 			hidden_in_tree = true;
 		} else {
-			node['_meta']['node'].find('.expand').text('^');
+			node.element.find('.expand').text('^');
 		}
 	}
 
-	for (var i in node) {
-		if (i != '_meta') {
-			refresh_visibility(node[i], hidden_in_tree);
-		}
+	for (var i in node.children) {
+		refresh_visibility(node.children[i], hidden_in_tree);
 	}
 }
 
 function expand_all(el) {
-	if ('_meta' in el && 'hidden' in el['_meta']) {
-		delete(el['_meta']['hidden']);
-	}
-	for(var i in el) {
-		if (i != '_meta') {
-			expand_all(el[i]);
-		}
+	el.hidden = false;
+	for(var i in el.children) {
+		expand_all(el.children[i]);
 	}
 }
 
 function clear_paper(el) {
 	var line;
-	while (line = el['_meta']['lines'].pop()) {
+	while (line = el.lines.pop()) {
 		line.remove();
 	}
 
-	for (var i in el) {
-		if (i != '_meta') {
-			clear_paper(el[i]);
-		}
+	for (var i in el.children) {
+		clear_paper(el.children[i]);
 	}
 }
 
 function redraw(formatted_data, options) {
 	refresh_visibility(formatted_data);
 
-	old_width = formatted_data['_meta']['width'];
+	old_width = formatted_data.width;
 
 	var changed = (old_width != calculate_width(formatted_data));
 	changed = calculate_positions(formatted_data) || changed;
 
 	if (options && 'follow' in options) {
 		if (changed && follow_on_move.is(':checked')) {
-			var node_position = options['follow']['_meta']['node'].offset();
+			var node_position = options['follow'].element.offset();
 
-			node_position.left += (options['follow']['_meta']['width']/2) - 100; // 200 is the width of the inner-node
+			node_position.left += (options['follow'].width/2) - 100; // 200 is the width of the inner-node
 
 			var top_offset = (0-($(window).height()/2));
 			var left_offset =(0-($(window).width()/2));
@@ -272,10 +248,8 @@ function redraw(formatted_data, options) {
 
 	if (changed) {
 
-		for (var i in formatted_data) {
-			if (i != '_meta') {
-				clear_paper(formatted_data[i]);
-			}
+		for (var i in formatted_data.children) {
+			clear_paper(formatted_data.children[i]);
 		}
 
 		var node_callbacks = 0;
@@ -284,10 +258,8 @@ function redraw(formatted_data, options) {
 			node_callbacks += 1;
 			if (node_callbacks == node_count - 1) {
 
-				for (var i in formatted_data) {
-					if (i != '_meta') {
-						draw_lines(formatted_data[i]);
-					}
+				for (var i in formatted_data.children) {
+						draw_lines(formatted_data.children[i]);
 				}
 				
 
@@ -313,15 +285,13 @@ $(function() {
 
 		calculate_width(formatted_data);
 
-		formatted_data['_meta']['left'] = 0;
+		formatted_data.left = 0;
 		calculate_positions(formatted_data);
 
 		place_nodes(formatted_data);
 		
-		for (var i in formatted_data) {
-			if (i != '_meta') {
-				draw_lines(formatted_data[i]);
-			}
+		for (var i in formatted_data.children) {
+			draw_lines(formatted_data.children[i]);
 		}
 
 		$('#menu .collapse').click(function() {
@@ -345,13 +315,7 @@ $(function() {
 		$('.node .inner-node .expand').click(function() {
 			var node_el = $(this).closest('.node');
 			var node = node_el.data('node');
-			if (node['_meta']['hidden']) {
-				// Show children
-				delete(node['_meta']['hidden']);
-			} else {
-				// Hide children
-				node['_meta']['hidden'] = true;
-			}
+			node.hidden = !node.hidden;
 
 			redraw(formatted_data, {follow: node});
 		});
